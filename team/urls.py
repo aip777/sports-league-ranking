323be7bb336dc12f7678092dfb398b6553d13229
Ariful Islam Palash <ariful.islam@bracits.com>
2023-04-05 03:34:56 +0600

    update:urls.py

--- team/urls.py
+++ team/urls.py
@@ -5,6 +5,8 @@ from team.view.ranking import (
     rankinglistView,
     deleteRakingView,
     updateRankingView,
+    TeamRankingView,
+    TeamRankingUpdateView
 )
 from .document.document_view import error_log
 
@@ -12,7 +14,7 @@ urlpatterns = [
     path("", csv_upload, name="upload_csv_file"),
     path("error-log/", error_log, name="error_log"),
     path("add-ranking/", addRankingView, name="add-ranking"),
-    path("list-ranking/", rankinglistView, name="ranking-list"),
-    path("update-ranking/<int:id>/", updateRankingView, name="update-ranking"),
+    path("ranking-list/", TeamRankingView.as_view(), name="ranking-list"),
+    path("update-ranking/<pk>/", TeamRankingUpdateView.as_view(), name="update-ranking"),
     path("delete-ranking/<int:id>/", deleteRakingView, name="ranking-delete"),
 ]
